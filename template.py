fac6daf50c91dd25eaa0f8f7b00d16f2cd75f0bd
a.tyagi <a.tyagi@tu-braunschweig.de>
2025-06-24 10:17:36 +0200

    Data Ingestion Done

--- template.py
+++ template.py
@@ -32,7 +32,6 @@ list_of_files = [
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    print(filedir,"---", filename)
     
     if filedir != "":
         os.makedirs(filedir, exist_ok = True)
