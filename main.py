487f3128eaf04e499dcc39eacaa3b56890001f57
a.tyagi <a.tyagi@tu-braunschweig.de>
2025-06-25 22:20:11 +0200

    transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -20,6 +21,18 @@ try:
     logger.info(f" >>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f" >>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f" >>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f" >>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<")
 
 except Exception as e:
